9affe352924b9ff632102c7c149fa95d5b039c9d
Maruf <marufbinawal@gmail.com>
2019-05-17 13:26:27 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
-
-def countdown
+def countdown with sleep(5)
+    counter = 0
+    while counter > 0
+      puts "#{counter} SECOND(S)!"
+      counter += 1
+        until countdown == 12 
+        p "HAPPY NEW YEAR!"
+    end 
 end
+
+
